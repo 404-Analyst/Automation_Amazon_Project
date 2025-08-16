71afe563dd48fe61b436a951c4e3e8949e6ce54f
Sujan Dhali <sujandhali08@gmail.com>
2025-08-16 22:48:27 +0530

    New Code added

--- src/test/java/TestNG/LoginTest.java
+++ src/test/java/TestNG/LoginTest.java
@@ -8,6 +8,43 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class LoginTest {
+public class LoginTest extends TestBase  {
+	
+	public void login() {
+		setup();// calling the TestBase class
+		
+	}
+		
+		public void Logindetails() throws InterruptedException {
+			
+			driver.findElement(By.xpath("//span[text()='Hello, sign in']")).click();
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//input[@name='email']")).sendKeys("8637556686");
+			Thread.sleep(2000);
+			//clicked on continue button 
+			driver.findElement(By.xpath("//input[@type='submit']")).click();
+			driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Sujan%2025");
+			Thread.sleep(2000);
+			driver.findElement(By.xpath("//input[@type='submit']")).click();
+			Thread.sleep(7000);
+		
+			System.out.println("Login test get successfull");
+		teardown();
+		
+			System.out.println("Browser closed");
+		
+	}
+
+	
+	public static void main(String[] arg) throws InterruptedException {
+		
+		LoginTest lg= new LoginTest();
+		lg.login();
+		lg.Logindetails();
+		
+	}
+	
+	
+	
 
 }
---
+++ src/test/java/TestNG/TestBase.java
@@ -0,0 +1,30 @@
+package TestNG;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestBase {
+	
+	public WebDriver driver;
+	
+	public void setup() {
+		WebDriverManager.chromedriver().setup();
+		driver =new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.amazon.in/");
+		System.out.println("Browser launced successfully");
+	}
+		public void teardown() {
+			
+			driver.quit();
+			
+		}
+	}
+	
+
+
+
