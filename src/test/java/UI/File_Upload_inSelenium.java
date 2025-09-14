182e6b7513ee71fbe29aa7403f5bb523088254b4
Sujan Dhali <sujandhali08@gmail.com>
2025-09-15 00:12:10 +0530

    Commit

--- src/test/java/UI/File_Upload_inSelenium.java
+++ src/test/java/UI/File_Upload_inSelenium.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class File_Upload_inSelenium {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         // ...your code...
@@ -15,6 +15,7 @@ public class File_Upload_inSelenium {
         driver.manage().window().maximize();
         driver.findElement(org.openqa.selenium.By.xpath("//input[@id='myFile']")).sendKeys("C:\\Users\\sujan\\Downloads\\Suta.pdf");
         System.out.println("File Uploaded Successfully");
+        Thread.sleep(5000);
         driver.quit();
     }
 }
