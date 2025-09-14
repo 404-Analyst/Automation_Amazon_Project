cf9c506d1d6d1cffd098f7a42ba89dfbb55290bf
Sujan Dhali <sujandhali08@gmail.com>
2025-09-15 00:19:24 +0530

    Sujan

--- src/test/java/AmazoneWebApp/TestBase.java
+++ src/test/java/AmazoneWebApp/TestBase.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.Test;
 
 public class TestBase {
 	
