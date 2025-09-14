809021bfef88370189832fa4198be9d9bcfaf210
Sujan Dhali <sujandhali08@gmail.com>
2025-09-14 18:10:19 +0530

    commit

---
+++ src/test/java/UI/File_Upload_inSelenium.java
@@ -0,0 +1,20 @@
+package UI;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class File_Upload_inSelenium {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        // ...your code...
+        driver.get("https://www.w3schools.com/howto/howto_html_file_upload_button.asp");
+
+        driver.manage().window().maximize();
+        driver.findElement(org.openqa.selenium.By.xpath("//input[@id='myFile']")).sendKeys("C:\\Users\\sujan\\Downloads\\Suta.pdf");
+        System.out.println("File Uploaded Successfully");
+        driver.quit();
+    }
+}
